a9b6a460535fbd480c7091195d944e7fc966aafd
HackerMisterH <143317889+HackerMisterH@users.noreply.github.com>
2023-09-24 17:49:10 +0600

    Add files via upload

---
+++ zip-crack_install.py
@@ -0,0 +1,8 @@
+from os import system
+system("clear")
+system("pip install zipfile36")
+system("pip install pyfiglet")
+system("pip install termcolor")
+system("mv Document zip-crack.py")
+print("Now run python zip-crack.py")
+system("rm -rf zip-crack_install.py")
